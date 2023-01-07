bf9e4c67fbfae988378c080dda8bbb2e4815a937
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-07 05:42:16 +0530

    Change content

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,9 @@ function Navbar() {
     <div className="text-3xl">
       Notice: This domain is for sale
       <p className="px-3">
+        email: blsgrn@tutanota.com
         <a href="https://dan.com/buy-domain/heavyarena.com">
-          click here to buy!!
+          click here to buy to using third party broker !!
         </a>
       </p>
     </div>
