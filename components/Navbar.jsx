beda3b26aa51f4155fc912a2fae9d1998a4e345f
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-07 05:39:15 +0530

    Create Navbar.jsx with TailwindCSS

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="text-3xl">
+      Notice: This domain is for sale
+      <p className="px-3">
+        <a href="https://dan.com/buy-domain/heavyarena.com">
+          click here to buy!!
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Navbar;
