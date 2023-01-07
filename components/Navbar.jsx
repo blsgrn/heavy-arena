161d91b37fa7ec5e4f0bc8853505e7aa87b6c95f
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-07 06:14:35 +0530

    change advert sertence

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="text-3xl">
-      Notice: This domain is for sale
-      <p className="px-3">
-        email: blsgrn@tutanota.com
-        <a href="https://dan.com/buy-domain/heavyarena.com">
-          click here to buy to using third party broker !!
-        </a>
-      </p>
+    <div>
+      <div>
+        Notice: This domain is for sale. <br /> email: blsgrn@tutanota.com
+        <p>
+          To buy through dan.com brokers,
+          <a href="https://dan.com/buy-domain/heavyarena.com"> click here!!</a>
+        </p>
+      </div>
     </div>
   );
 }
